080b2780eed1cfd908dd245a240cd4f739c44048
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-02 20:36:42 +0000

    added back more functionality for processing the data

--- utils/amazon_dataset.py
+++ utils/amazon_dataset.py
@@ -1,32 +1,52 @@
 from torchtext.data import TabularDataset 
+from torch.utils.data import Dataset, DataLoader
 
-class AmazonReviewsDataset(TabularDataset):
-    
-    urls = [
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Books_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Electronics_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Movies_and_TV_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_CDs_and_Vinyl_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Clothing_Shoes_and_Jewelry_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Home_and_Kitchen_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Kindle_Store_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Sports_and_Outdoors_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Health_and_Personal_Care_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Toys_and_Games_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Video_Games_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Tools_and_Home_Improvement_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Beauty_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Apps_for_Android_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Office_Products_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Pet_Supplies_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Automotive_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Grocery_and_Gourmet_Food_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Patio_Lawn_and_Garden_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Baby_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Digital_Music_5.json.gz',
-#            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Musical_Instruments_5.json.gz',
-            'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Amazon_Instant_Video_5.json.gz',
-        ]
-    name='amazonreviews'
-    dirname='processed'
+class AmazonDataset(Dataset):
+    def __init__(self, data_list, max_inp_length=None, use_cuda=True):
+        """
+        data_list is a list of tuples: (x,y) where x is a list of ids and y is a label
+        """
+        self.data = data_list
+        self.max_len = max_inp_length
+        self.data_tensors = []
+        device = torch.device("cuda" if (torch.cuda.is_available() and use_cuda) else "cpu")
+        for (i, t) in tqdm_notebook(self.data):
+            print(i, t)
+            self.data_tensors.append((torch.LongTensor(i[:self.max_len]).to(device), \
+                                        torch.LongTensor([t]).to(device)))
+                
+    def __getitem__(self, key):
+        (inp, tgt) = self.data_tensors[key]
+        
+        return inp, tgt, len(inp)
+
+    def __len__(self):
+        return len(self.data)
+
+    def pad(tensor, length, dim=0, pad=0):
+        """Pad tensor to a specific length.
+        :param tensor: vector to pad
+        :param length: new length
+        :param dim: (default 0) dimension to pad
+        :returns: padded tensor if the tensor is shorter than length
+        """
+        if tensor.size(dim) < length:
+            return torch.cat(
+                [tensor, tensor.new(*tensor.size()[:dim],
+                                    length - tensor.size(dim),
+                                    *tensor.size()[dim + 1:]).fill_(pad)],
+                dim=dim)
+        else:
+            return tensor
+
+    def batchify(batch):
+        maxlen = max(batch, key = itemgetter(2))[-1]
+        batch_list = []
+        target_list = []
+        for b in batch:
+            batch_list.append(pad(b[0], maxlen, dim=0, pad=PAD_IDX))
+            target_list.append(b[1])
+        input_batch = torch.stack(batch_list, 0)
+        target_batch = torch.stack(target_list, 0)
+
+        return input_batch, target_batch
