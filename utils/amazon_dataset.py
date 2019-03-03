c233c48ecfa3390a224e018663ecbfa111d22156
raileanu <raileanu.roberta@gmail.com>
2019-03-03 10:54:52 -0500

    more changes

--- utils/amazon_dataset.py
+++ utils/amazon_dataset.py
@@ -1,6 +1,7 @@
 import torch
 from torchtext.data import TabularDataset 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+from tqdm import tqdm_notebook
 
 class AmazonDataset(Dataset):
     def __init__(self, data_list, max_inp_length=None, use_cuda=True):
@@ -23,30 +24,30 @@ class AmazonDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def pad(tensor, length, dim=0, pad=0):
-        """Pad tensor to a specific length.
-        :param tensor: vector to pad
-        :param length: new length
-        :param dim: (default 0) dimension to pad
-        :returns: padded tensor if the tensor is shorter than length
-        """
-        if tensor.size(dim) < length:
-            return torch.cat(
-                [tensor, tensor.new(*tensor.size()[:dim],
-                                    length - tensor.size(dim),
-                                    *tensor.size()[dim + 1:]).fill_(pad)],
-                dim=dim)
-        else:
-            return tensor
+def pad(tensor, length, dim=0, pad=0):
+    """Pad tensor to a specific length.
+    :param tensor: vector to pad
+    :param length: new length
+    :param dim: (default 0) dimension to pad
+    :returns: padded tensor if the tensor is shorter than length
+    """
+    if tensor.size(dim) < length:
+        return torch.cat(
+            [tensor, tensor.new(*tensor.size()[:dim],
+                                length - tensor.size(dim),
+                                *tensor.size()[dim + 1:]).fill_(pad)],
+            dim=dim)
+    else:
+        return tensor
 
-    def batchify(batch):
-        maxlen = max(batch, key = itemgetter(2))[-1]
-        batch_list = []
-        target_list = []
-        for b in batch:
-            batch_list.append(pad(b[0], maxlen, dim=0, pad=PAD_IDX))
-            target_list.append(b[1])
-        input_batch = torch.stack(batch_list, 0)
-        target_batch = torch.stack(target_list, 0)
+def batchify(batch):
+    maxlen = max(batch, key = itemgetter(2))[-1]
+    batch_list = []
+    target_list = []
+    for b in batch:
+        batch_list.append(pad(b[0], maxlen, dim=0, pad=PAD_IDX))
+        target_list.append(b[1])
+    input_batch = torch.stack(batch_list, 0)
+    target_batch = torch.stack(target_list, 0)
 
-        return input_batch, target_batch
+    return input_batch, target_batch
