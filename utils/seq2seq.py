d6cbdd967381620de303b3d1f96012862d88a65c
Ilia Kulikov <kulikov@cs.nyu.edu>
2019-03-07 16:30:49 -0500

    att fixes

--- utils/seq2seq.py
+++ utils/seq2seq.py
@@ -193,6 +193,48 @@ class AttentionLayer(nn.Module):
         return out, attn_weights
 
 
+class IBMAttentionLayer(nn.Module):
+
+    def __init__(self, dimensions, attention_type='general'):
+        super().__init__()
+        if attention_type not in ['dot', 'general']:
+            raise ValueError('Invalid attention type selected.')
+
+        self.attention_type = attention_type
+        if self.attention_type == 'general':
+            self.linear_in = nn.Linear(dimensions, dimensions, bias=False)
+
+        self.linear_out = nn.Linear(dimensions*2, dimensions, bias=False)
+        self.softmax = nn.Softmax(dim=-1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, query, context):
+
+        query = query.transpose(0,1)
+        context = context.transpose(0,1)
+        batch_size, output_len, dimensions = query.size()
+        context_len = context.size(1)
+
+        if self.attention_type == 'general':
+            query = query.view(batch_size * output_len, dimensions)
+            query = self.linear_in(query)
+            query = query.view(batch_size, output_len, dimensions)
+
+        attention_scores = torch.bmm(query, context.transpose(1,2).contiguous())
+        attention_scores = attention_scores.view(batch_size*output_len, context_len)
+        attention_weights = self.softmax(attention_scores)
+        attention_weights = attention_weights.view(batch_size, output_len, context_len)
+
+        mix = torch.bmm(attention_weights, context)
+        combined = torch.cat((mix, query), dim=2)
+        combined = combined.view(batch_size*output_len, 2*dimensions)
+
+        output = self.linear_out(combined).view(batch_size, output_len, dimensions)
+        output = self.tanh(output)
+
+        return output, attention_weights
+
+
 class DecoderRNN(nn.Module):
     """Generates a sequence of tokens in response to context."""
 
@@ -214,7 +256,8 @@ class DecoderRNN(nn.Module):
         self.gru = nn.GRU(
             self.embed_size, self.hidden_size, num_layers=self.num_layers, batch_first=True, dropout=dropout if num_layers > 1 else 0,
         )
-        self.attention = AttentionLayer(self.hidden_size) if attention_flag else None;
+        self.attention = IBMAttentionLayer(self.hidden_size) if attention_flag else None;
+
         self.out = nn.Linear(self.hidden_size, self.vocab_size)
 
     def forward(self, text_vec, decoder_hidden, encoder_states):
@@ -232,7 +275,7 @@ class DecoderRNN(nn.Module):
         for i in range(seqlen):
             decoder_output, decoder_hidden = self.gru(emb[:,i,:].unsqueeze(1), decoder_hidden)
             if self.attention is not None:
-                decoder_output_attended, attn_weights = self.attention(decoder_output, decoder_hidden, encoder_states)            
+                decoder_output_attended, attn_weights = self.attention(decoder_hidden[-1].unsqueeze(0), encoder_states[0])
                 output.append(decoder_output_attended)
                 attn_w_log.append(attn_weights)
             else:
