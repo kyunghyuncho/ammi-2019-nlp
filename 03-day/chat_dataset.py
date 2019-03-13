5b1ed3480abb29c7703fbaa01d444ed90505fa65
Ilia Kulikov <kulikov@cs.nyu.edu>
2019-03-13 04:57:35 -0400

    no tqdm_notebook

--- 03-day/chat_dataset.py
+++ 03-day/chat_dataset.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import re
 from collections import Counter, namedtuple
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
 from global_variables import SOS_IDX, SOS_TOKEN, EOS_IDX, EOS_TOKEN, UNK_IDX, UNK_TOKEN, PAD_IDX, PAD_TOKEN, SEP_IDX, SEP_TOKEN, device
 
 RETOK = re.compile(r'\w+|[^\w\s]|\n', re.UNICODE)
